8ab4b32ee9336735de61f1bc0b8d9317179679e0
Hyx <75287447+moisesxtian@users.noreply.github.com>
2022-10-24 15:37:26 +0800

    Add files via upload

---
+++ StudentInfoApplication/StudentInfoApplication/frmStudentInfo.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace StudentInfoApplication
+{
+    partial class frmStudentInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb1 = new System.Windows.Forms.ListBox();
+            this.lb3 = new System.Windows.Forms.ListBox();
+            this.lb2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.TextBox();
+            this.ln = new System.Windows.Forms.TextBox();
+            this.fn = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(63)))), ((int)(((byte)(75)))));
+            this.lb1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lb1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.ForeColor = System.Drawing.SystemColors.Window;
+            this.lb1.FormattingEnabled = true;
+            this.lb1.ItemHeight = 15;
+            this.lb1.Location = new System.Drawing.Point(30, 127);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(120, 90);
+            this.lb1.TabIndex = 0;
+            // 
+            // lb3
+            // 
+            this.lb3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(63)))), ((int)(((byte)(75)))));
+            this.lb3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lb3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb3.ForeColor = System.Drawing.SystemColors.Window;
+            this.lb3.FormattingEnabled = true;
+            this.lb3.ItemHeight = 15;
+            this.lb3.Location = new System.Drawing.Point(302, 127);
+            this.lb3.Name = "lb3";
+            this.lb3.Size = new System.Drawing.Size(120, 90);
+            this.lb3.TabIndex = 1;
+            // 
+            // lb2
+            // 
+            this.lb2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(63)))), ((int)(((byte)(75)))));
+            this.lb2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lb2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb2.ForeColor = System.Drawing.SystemColors.Window;
+            this.lb2.FormattingEnabled = true;
+            this.lb2.ItemHeight = 15;
+            this.lb2.Location = new System.Drawing.Point(166, 127);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(120, 90);
+            this.lb2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(231)))), ((int)(((byte)(235)))));
+            this.label1.Location = new System.Drawing.Point(26, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Student ID *";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(231)))), ((int)(((byte)(235)))));
+            this.label2.Location = new System.Drawing.Point(156, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Last Name *";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(231)))), ((int)(((byte)(235)))));
+            this.label3.Location = new System.Drawing.Point(298, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "First Name *";
+            // 
+            // id
+            // 
+            this.id.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(63)))), ((int)(((byte)(75)))));
+            this.id.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.id.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.id.Location = new System.Drawing.Point(30, 41);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(120, 13);
+            this.id.TabIndex = 6;
+            // 
+            // ln
+            // 
+            this.ln.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(63)))), ((int)(((byte)(75)))));
+            this.ln.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ln.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ln.Location = new System.Drawing.Point(160, 41);
+            this.ln.Name = "ln";
+            this.ln.Size = new System.Drawing.Size(126, 13);
+            this.ln.TabIndex = 7;
+            // 
+            // fn
+            // 
+            this.fn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(63)))), ((int)(((byte)(75)))));
+            this.fn.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.fn.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.fn.Location = new System.Drawing.Point(302, 41);
+            this.fn.Name = "fn";
+            this.fn.Size = new System.Drawing.Size(120, 13);
+            this.fn.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(65)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(183, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 40);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(41)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(447, 239);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.fn);
+            this.Controls.Add(this.ln);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb2);
+            this.Controls.Add(this.lb3);
+            this.Controls.Add(this.lb1);
+            this.Name = "Form1";
+            this.Text = "frmStudentInfo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lb1;
+        private System.Windows.Forms.ListBox lb3;
+        private System.Windows.Forms.ListBox lb2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.TextBox ln;
+        private System.Windows.Forms.TextBox fn;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ StudentInfoApplication/StudentInfoApplication/frmStudentInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentInfoApplication
+{
+
+    public partial class frmStudentInfo : Form
+    {
+
+    public frmStudentInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input1, input2, input3;
+            input1 = id.Text;
+            input2 = ln.Text;
+            input3 = fn.Text;
+        
+            student myclass = new student(input1,input2,input3);
+            string set1, set2, set3;
+            set1 = myclass.getID();
+            set2 = myclass.getfn();
+            set3 = myclass.getln();
+            lb1.Items.Add(set1);
+            lb3.Items.Add(set2);
+            lb2.Items.Add(set3);
+
+
+        }
+    }
+}
